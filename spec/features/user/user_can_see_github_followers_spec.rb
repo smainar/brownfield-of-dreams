a1aa5f4c07003365fc36f22ac70308899e61427c
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-21 16:17:03 -0600

    Add feature test for User Dashboard: Github Followers
    Co-Authored-By: Mills Provosty <MillsProvosty@users.noreply.github.com>

---
+++ spec/features/user/user_can_see_github_followers_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'A User on dashboard page' do
+ it 'can see github followers' do
+   VCR.use_cassette('user_github_followers', record: :new_episodes) do
+     user = create(:user)
+     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+     visit '/dashboard'
+
+     expect(page).to have_content('Followers')
+     expect(page).to have_css('.github-followers', count: 5)
+
+     within(first('.github-followers')) do
+       expect(page).to have_link("kylecornelissen")
+     end
+   end
+ end
+end
