5e2683ae576b346f6815de3f4895b567991effef
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-21 16:43:19 -0600

    Adds spec

---
+++ spec/features/user/can_see_following_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'As a User on my dashboard page' do
+  it 'can see github members Im following' do
+    VCR.use_cassette('user_github_following', record: :new_episodes) do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+
+      expect(page).to have_content('Following')
+      expect(page).to have_css('.github-following', count: 5)
+
+      within(first('.github-following')) do
+        expect(page).to have_link("kylecornelissen")
+      end
+    end
+  end
+end
