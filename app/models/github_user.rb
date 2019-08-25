17110bb1a065e0365cbf62d3100dd5d526caaeb7
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-24 20:14:55 -0600

    Work in progress - add friend? and has_github_account? methods to GithubUser PORO
    Co-Authored-By: Mills Provosty <MillsProvosty@users.noreply.github.com>
    Co-Authored-By: Matt Levy <milevy1@gmail.com>

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,9 +1,21 @@
 class GithubUser
-  attr_reader :handle, 
+  attr_reader :handle,
               :url
 
   def initialize(github_profile)
     @handle = github_profile[:login]
     @url = github_profile[:html_url]
+  end
+
+  def friend?(current_user)
+    if !friend_finder(current_user) && self.handle
+    end
+  end
+
+  def has_github_account?(current_user)
+    follower = User.find_by(github_handle: self.handle)
+    if follower && Friendship.find_by(user_id: current_user.id, friend_id: follower.id) || Friendship.find_by(friend_id: current_user.id, user_id: follower.id)
+
+    end
   end
 end
