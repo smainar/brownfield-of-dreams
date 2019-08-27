d98ae0c4575daab61b50b43b4a4b88f509c1e616
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-27 15:52:53 -0600

    Fixes usermailer spe c

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 RSpec.describe UserMailer, type: :mailer do
-  describe 'activation email after registration' do
+  describe 'sends email at registration' do
     before :each do
       @user = create(:user)
       @mail = UserMailer.activation_email(@user)
     end
+
+
   end
 end
