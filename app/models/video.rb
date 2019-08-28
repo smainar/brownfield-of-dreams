b91a7fca286936f6bb82df49f116c15f98491416
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-28 04:41:10 -0600

    Delete test and bookmarked_videos (class method) in Video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,11 +2,4 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
-
-  def self.bookmarked_videos(user_id)
-    self.select("videos.*")
-      .joins(:user_videos)
-      .where("user_videos.user_id = ?", user_id)
-      .order(:tutorial_id, :position)
-  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,21 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  describe "class methods" do
-    it "::bookmarked_videos" do
-      user = create(:user)
-
-      tutorial_1 = create(:tutorial)
-
-      video_1 = create(:video, title: "Title 1", tutorial: tutorial_1, position: 0)
-      video_2 = create(:video, title: "Title 2", tutorial: tutorial_1, position: 2)
-      video_3 = create(:video, title: "Title 3", tutorial: tutorial_1, position: 1)
-
-      user_video_1 = create(:user_video, user: user, video: video_1)
-      user_video_2 = create(:user_video, user: user, video: video_2)
-      user_video_3 = create(:user_video, user: user, video: video_3)
-
-      expect(Video.bookmarked_videos(user.id)).to eq([video_1, video_3, video_2])
-    end
-  end
 end
