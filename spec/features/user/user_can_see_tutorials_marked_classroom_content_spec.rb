9d3ea798e2ad621ed91fe75f923cbf55ee033b92
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-27 20:50:06 -0600

    Add feature test - all tutorials are viewable to logged in users

---
+++ spec/features/user/user_can_see_tutorials_marked_classroom_content_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe "As a logged in user", type: :feature do
+  describe "Classroom Content" do
+    it "tutorials marked as 'classroom content' are viewable only if the user is logged in" do
+      VCR.use_cassette('logged_in_user_sees_classroom_content', record: :new_episodes) do
+        user = create(:user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        tutorial_1 = create(:tutorial, classroom: true)
+        tutorial_2 = create(:tutorial, classroom: true)
+        tutorial_3 = create(:tutorial, classroom: false)
+
+        video_1 = create(:video, tutorial: tutorial_1)
+        video_2 = create(:video, tutorial: tutorial_2)
+        video_3 = create(:video, tutorial: tutorial_3)
+
+        visit root_path
+
+        expect(page).to have_content(tutorial_1.title)
+        expect(page).to have_content(tutorial_2.title)
+        expect(page).to have_content(tutorial_3.title)
+      end
+    end
+  end
+end
