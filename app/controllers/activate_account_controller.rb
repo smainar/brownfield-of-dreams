983e7e687db3a3b31e73f146f877c00004e0184a
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-08-29 11:26:02 -0600

    Will have to redo rubocop

--- app/controllers/activate_account_controller.rb
+++ app/controllers/activate_account_controller.rb
@@ -4,6 +4,7 @@ class ActivateAccountController < ApplicationController
   def create
     user = User.find(params[:user_id])
     user.update(active: true)
+    binding.pry
     flash[:success] = 'Thank you! Your account is now activated.'
     redirect_to dashboard_path
   end
